a43bf7310d0ff715a51e505d1ca3db598188ac21
Michal Przytulski <michal@przytulski.pl>
2013-08-27 00:37:13 +0200

    navigation

--- elements/pages/phpspec.php
+++ elements/pages/phpspec.php
@@ -1,3 +1,6 @@
+<h2>Behat</h2>
+<h3><a href="behat/">Description</a> | <a href="behat/screenshots">Screenshots</a></h3>
+
 <p>Missing spec quick fix</p>
 <img src="./images/screenshots/phpspec/missing_spec_quick_fix.png" />
 <p>Missing spec inspection</p>
