51a323cfa6ea98c142a668ac4b17721bb217b15d
Michal Przytulski <michal@przytulski.pl>
2013-08-26 23:32:04 +0100

    + screenshots

---
+++ elements/pages/behat.php
@@ -0,0 +1,23 @@
+<p>Feature annotations completion</p>
+<img src="./images/screenshots/behat/context_annotations.png" />
+<p>Sub context name type completion</p>
+<img src="./images/screenshots/behat/context_name_provider.png" />
+<p>Context return type provider</p>
+<img src="./images/screenshots/behat/context_type_hinting.png" />
+<p>Feature file generator file</p>
+<img src="./images/screenshots/behat/feature_form.png" />
+<p>Feature file Behat tags completion</p>
+<img src="./images/screenshots/behat/feature_tags.png" />
+<p>Behat and Page Object extension file templates</p>
+<img src="./images/screenshots/behat/file_templates.png" />
+<img src="./images/screenshots/behat/file_templates_context.png" />
+<p>Gherkin step reference provider</p>
+<img src="./images/screenshots/behat/gherkin_step_reference.png" />
+<p>Sub context, Page Object and Elements inspection</p>
+<img src="./images/screenshots/behat/page_object_name_inspection.png" />
+<p>Quick fix for missing Page Objects</p>
+<img src="./images/screenshots/behat/page_object_quick_fix.png" />
+<p>Page Object and Elements reference provider</p>
+<img src="./images/screenshots/behat/page_object_reference.png" />
+<p>Custom icons for Behat related files</p>
+<img src="./images/screenshots/behat/project_browser_custom_icon.png" />
---
+++ elements/pages/phpspec.php
@@ -0,0 +1,8 @@
+<p>Missing spec quick fix</p>
+<img src="./images/screenshots/phpspec/missing_spec_quick_fix.png" />
+<p>Missing spec inspection</p>
+<img src="./images/screenshots/phpspec/missing_spec_inspection.png" />
+<p>Navigation</p>
+<img src="./images/screenshots/phpspec/nagivation.png" />
+<p>Type hint</p>
+<img src="./images/screenshots/phpspec/type_hint.png" />
