4cb25c27a8d99a871de9e45862ddfc9f4df71f9d
Michal Przytulski <michal@przytulski.pl>
2013-08-26 23:11:00 +0100

    updating page

---
+++ elements/right_col.php
@@ -0,0 +1,18 @@
+<h1>PhpStorm Plugins</h1>
+<p></p>
+
+<h2>Behat plugin for PhpStorm</h2>
+<ul>
+  <li><a href="https://github.com/mprzytulski/phpstorm-behat/zipball/master">Download <strong>ZIP File</strong></a></li>
+  <li><a href="https://github.com/mprzytulski/phpstorm-behat/tarball/master">Download <strong>TAR Ball</strong></a></li>
+  <li><a href="https://github.com/mprzytulski/phpstorm-behat">View On <strong>GitHub</strong></a></li>
+</ul>
+<p class="view"><a href="./files/behat-0.3.2.zip">Download letest version <small>0.3.2</small></a></p>
+<p>&nbsp;</p>
+<h2>phpspec plugin for PhpStorm</h2>
+<ul>
+  <li><a href="https://github.com/mprzytulski/phpstorm-phpspec/zipball/master">Download <strong>ZIP File</strong></a></li>
+  <li><a href="https://github.com/mprzytulski/phpstorm-phpspec/tarball/master">Download <strong>TAR Ball</strong></a></li>
+  <li><a href="https://github.com/mprzytulski/phpstorm-phpspec">View On <strong>GitHub</strong></a></li>
+</ul>
+<p class="view"><a href="./files/phpspec-0.1.zip">Download letest version <small>0.1</small></a></p>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Welcome to home of PhpStorm BDD plugins.</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+		  <?php include 'elements/right_column.php'; ?>
+      </header>
+      <section>
+        <h2><a name="behat" class="anchor" href="#behat"><span class="octicon octicon-link"></span></a>Behat / <a href="./behat">Screenshots</a></h2>
+		<h4>General:</h4>
+		<ul>
+			<li>Custom icon for Behat Context and PageObjects files</li>
+		</ul>
+
+		<h4>Feature file:</h4>
+		<ul>
+			<li>tag auto completion</li>
+			<li>reference provider for Gherkin steps</li>
+		</ul>
+
+		<h4>Behat context:</h4>
+		<ul>
+			<li>reference and inspection provider for PageObjcet names</li>
+			<li>reference and inspection provider for SubContext names</li>
+			<li>create PageObject quick fix for missing classes</li>
+			<li>type hinting for getMainContext() method</li>
+			<li>type hinting for getSubContext() method</li>
+			<li>parameter auto completion for getSubContext() method with names of available contextes</li>
+			<li>annotation hinting</li>
+		</ul>
+
+		<h4>Page Objects extension:</h4>
+		<ul>
+			<li>quick fix for missing PageObjects and PageObject elements</li>
+			<li>reference provider for getElement and hasElement method of PageObject classes</li>
+			<li>type hinting for getPage() and getElement() methods</li>
+			<li>parameter auto completion for getPage() method with names of available pages in current project</li>
+		</ul>
+		
+		<hr />
+		
+		<h2><a name="phpspec" class="anchor" href="#phpspec"><span class="octicon octicon-link"></span></a>phpspec / <a href="./behat">Screenshots</a></h2>
+		<p>phpspec support for PhpStorm IDE.</p>
+
+		<h4>General:</h4>
+		<ul>
+			<li>Custom icons for phpspac files</li>
+			<li>Generate spec file for php class</li>
+		</ul>
+
+		<h4>Navigation:</h4>
+		<ul>
+			<li>go to spec from php class / go to clas from spec class</li>
+		</ul>
+
+      </section>
+      <footer>
+        <p>This project is maintained by <a href="https://github.com/mprzytulski">mprzytulski</a></p>
+        <p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
+      </footer>
+    </div>
+    <script src="javascripts/scale.fix.js"></script>
+    
+  </body>
+</html>
